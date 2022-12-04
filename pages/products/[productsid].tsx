e8526173aae5f23ec6ac0acbc10aff253a591f74
unknown <eddie225561@gmail.com>
2022-12-04 20:22:15 +0200

    products css and drawer

--- pages/products/[productsid].tsx
+++ pages/products/[productsid].tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {use, useEffect, useState} from "react";
+import InfiniteScroll from "react-infinite-scroll-component"
+import Link from "next/link";
+import {Grid} from "@mui/material"
+import Drawer from "../../components/drawer/drawer"
 
-const product = () => {
-  return <div>one single product</div>;
+const productlist = () => {
+const  items = [1,2,3,4,5]
+
+  return (
+    <Grid container>
+      <Grid container><Grid item xs={1}></Grid><Grid item xs={2}>filter</Grid><Grid item xs={9}></Grid></Grid>
+      {items.map((item : any, i : any)=> { 
+        return <Grid key={i} container style={{border: "1px solid black", height: "30vh", width: '100vw'}}><Grid item xs={1}></Grid><Grid item xs={6}>box</Grid><Grid item xs={4}>Name<Grid item xs={1}></Grid></Grid></Grid>
+        })}
+      <Drawer />
+    </Grid>
+  );
 };
 
-export default product;
+
+
+export default productlist;
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import InfiniteScroll from "react-infinite-scroller";
+import React, {use, useEffect, useState} from "react";
+import InfiniteScroll from "react-infinite-scroll-component"
 import Link from "next/link";
+import {Grid} from "@mui/material"
+import Drawer from "../../components/drawer/drawer"
 
 const productlist = () => {
+const  items = [1,2,3,4,5]
+
   return (
-    <div>
-      productlist <Link href={`/products/1`}>one product</Link>
-    </div>
+    <Grid container>
+      <Grid container><Grid item xs={1}></Grid><Grid item xs={2}>            <Drawer /></Grid><Grid item xs={9}></Grid></Grid>
+      {items.map((item : any, i : any)=> { 
+        return <Grid key={i} container style={{border: "1px solid black", height: "30vh", width: '100vw'}}><Grid item xs={1}></Grid><Grid item xs={6}>box</Grid><Grid item xs={4}>Name<Grid item xs={1}></Grid></Grid></Grid>
+        })}
+
+    </Grid>
   );
 };
+
+
 
 export default productlist;
