e355a891c14f069de69c63b3472bdf3cb796aec7
Eddie <placenamehere1901@gmail.com>
2022-12-28 21:04:47 +0200

    env change

--- database/db.ts
+++ database/db.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
