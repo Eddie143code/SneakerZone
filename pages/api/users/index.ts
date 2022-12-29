a9b63f5377be0c474ab60a313ea015e92f530698
Eddie <placenamehere1901@gmail.com>
2022-12-29 18:45:28 +0200

    removed 'users'

--- pages/api/users/index.ts
+++
@@ -1,6 +0,0 @@
-import { users } from "../../../data/users";
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function userHandler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(users);
-}
